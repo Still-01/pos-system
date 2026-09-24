adceb9c77651a884f27ad054b4e46a6313288d99
still-01 <3636551527@qq.com>
2026-09-24 22:06:03 +0800

    加入clear函数避免重复报错 改进算法 添加注释

--- main.c
+++ main.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
 #include <string.h>
-int main()
+char input[11]={0};//输入的字符串
+double price[3]={3.50, 0.50, 6.00};//价格数组
+int number[3]={0};//数量数组//通过数组实现了对多个变量的操控
+int id=0;
+char password[6]={0};
+char name[3][10]={"Cola", "Lollipop", "Noodles"};//商品名称数组
+ void  f(int a){
+            printf("%-10s% .2f\n", name[a], price[a]);//格式对齐
+            number[a]++;
+        }
+ void clear_input(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        // 把当前行剩余字符读走丢掉
+    }
+}   
+int main(void)
+ 
+
 {
-    char input[11]={0};
-    while(1)
+   printf("Welcome to the Pos-system!\nPlease enter the password:\n");
+   while(1){
+    scanf("%5s", password);
+    clear_input();
+   if(strcmp(password, "1037") == 0)
     {
-        scanf("%10s", input);
-        if(strcmp(input, "exit") == 0)
-        {
+    printf("Password correct!\nPlease enter the command:\n");
+    break;
+    }
+    else
+    {   
+        printf("Error: Incorrect password\nPlease enter the password again:\n");
+    }
+   }//密码系统
+
+     while(1)
+    {   scanf("%10s", input);
+        clear_input(); 
+        if(strcmp(input, "exit") == 0|| strcmp(input, "quit") == 0)
+        {printf("Thank you for using the Pos-system!\n");
             break;
         }
-        else if(strcmp(input, "001") == 0)
-        {
-            printf("Cola     3.50\n");
-        }
-        
-        else if(strcmp(input, "002") == 0)
-        {
-            printf("Lollipop    0.50\n");
-        }
-        else if(strcmp(input, "003") == 0)
-        {
-            printf("Noodles    6.00\n");
-        }
-        else if(strcmp(input,"prices") == 0)
+        else  if(input[0] == '0' && input[1] == '0' && strlen(input) == 3)//先讨论输入形式为00x
+        {if(input[2] >= '1' && input[2] <= '3')
+            {id = input[2] - '1';//将输入的字符转换为对应的数组下标
+            f(id);
+            }
+            else printf("Error: Item not found\n");}
+        else if(strcmp(input,"prices") == 0)//考虑输入的是其他指令
         {   printf("Item      No.        Pri.\n");
-            printf("Cola      001        3.50\n");
-            printf("Lollipop  002        0.50\n");
-            printf("Noodles   003        6.00\n");
-        }
-        else if(input[0] == '0' && input[1] == '0' && strlen(input) == 3)
-        {
-            printf("Error: Item not found\n");
-        }
-        else
-        {
-            printf("Error: Invalid input\n");
-}
-    }
-    return 0;
-}
+            printf("Cola      001        %.2f\n", price[0]);
+            printf("Lollipop  002        %.2f\n", price[1]);
+            printf("Noodles   003        %.2f\n", price[2]);
+        }//表格打印
+        else printf("Error: Invalid input\n");//无效指令
+      } 
+      return 0;
+} 
